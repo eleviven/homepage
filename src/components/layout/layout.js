c08257224f131294658c10cb3542c532fb7ee15a
eleviven <firuz.haciyeff@gmail.com>
2021-08-11 00:53:38 +0400

    Add footer to layout

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../header/header";
+import Footer from "../footer/footer";
 
 export default function Layout({ children }) {
   return (
     <React.Fragment>
       <Header />
       {children}
+      <Footer />
     </React.Fragment>
   );
 }
