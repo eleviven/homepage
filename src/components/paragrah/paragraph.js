3f73f427362802157b770bd50aec73be44a81e05
eleviven <firuz.haciyeff@gmail.com>
2021-08-11 00:53:54 +0400

    Paragraph component update

--- src/components/paragrah/paragraph.js
+++ src/components/paragrah/paragraph.js
@@ -3,14 +3,14 @@ import Button from "../button/button";
 export default function Paragraph({ title, showMore, onClickMore, ...props }) {
   return (
     <div
-      className="flex items-center justify-between border-b-2 border-secondary-light mb-6 pb-2"
+      className="flex items-center justify-between border-b-[3px] border-default dark:border-secondary-light mb-6 pb-2.5"
       {...props}
     >
-      <h2 className="text-primary-300 text-xl sm:text-3xl font-fira-code font-semibold">
+      <h2 className="text-default dark:text-primary-300 text-xl sm:text-3xl font-fira-code font-semibold">
         {title}
       </h2>
       {showMore && (
-        <Button title="View all" variant="secondary" onCick={onClickMore} />
+        <Button title="View all" variant="secondary" onClick={onClickMore} />
       )}
     </div>
   );
