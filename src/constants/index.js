02a09efc76d880f465af08309404380505194a7d
eleviven <firuz.haciyeff@gmail.com>
2021-08-12 21:53:08 +0400

    Add initial description to app contant

--- src/constants/index.js
+++ src/constants/index.js
@@ -13,5 +13,6 @@ export const DEVELOPER = {
 
 export const APP = {
   TITLE: DEVELOPER.NAME,
+  DESCRIPTION: "I'm Front end Developer. Sharing what I've learned here",
   DOMAIN: process.env.NEXT_PUBLIC_WEBSITE_URL,
 };
