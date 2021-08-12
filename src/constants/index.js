bcc35438beac0acf458b376bff36bfc8e12f79e1
eleviven <firuz.haciyeff@gmail.com>
2021-08-13 00:50:29 +0400

    Add developer image & description

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,9 @@ import { SvgGithub } from "../components/icons";
 
 export const DEVELOPER = {
   NAME: "Firuz Haciyev",
+  IMAGE: "/images/eleviven.jpeg",
+  DESCRIPTION:
+    "I'm a front end developer. I love sharing what I've learned and learning new things. Years from now, I hope this will be an informative blog for new developers.",
   SOCIAL: {
     GITHUB: {
       TITLE: "Github",
