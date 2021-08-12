d5ed247c1058fc593bea60e752c0a7a38d0321d8
eleviven <firuz.haciyeff@gmail.com>
2021-08-12 21:15:30 +0400

    Add accessoryLeft prop to button

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,11 +2,21 @@ import React from "react";
 import cn from "classnames";
 import styles from "./button.module.css";
 
+const renderComponent = (component) => {
+  switch (typeof component) {
+    case "function":
+      return component();
+    default:
+      return component;
+  }
+};
+
 export default function Button({
   title,
   variant,
   size,
   appearance,
+  accessoryLeft,
   className,
   children,
   ...props
@@ -22,6 +32,9 @@ export default function Button({
       )}
       {...props}
     >
+      {accessoryLeft && (
+        <span className="mr-2">{renderComponent(accessoryLeft)}</span>
+      )}
       {title ? title : children}
     </button>
   );
