844e85463e45cfc9828dc51263340255f0919996
eleviven <firuz.haciyeff@gmail.com>
2021-08-15 01:48:40 +0400

    Responsive bug fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,7 +73,7 @@ export default function Home({ articles, projects, githubUser }) {
         />
         <Post.List data={articles} className="section" />
         <Paragraph title="Projects" />
-        <section className="grid grid-cols-3 gap-12 py-3 section">
+        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12 py-3 section">
           {projects?.map((item) => (
             <ProjectItem
               key={item.id}
